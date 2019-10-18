e07397adb81b1d05e1651b1d057d6169e1775dd7
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-10-19 00:24:10 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   apps : [{
-    name: 'API',
+    name: 'Twitter-Discord',
     script: 'index.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
