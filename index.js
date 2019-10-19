8a5d9ac7c31086718ac6c8934eae100644a9e046
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2019-10-19 02:31:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -60,14 +60,14 @@ client.on('ready', () => {
                 if (old_avatar && old_avatar !== tweets[0].user.profile_image_url_https){
                     console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] avatar changed, setting in Discord...`)
                     client.user.setAvatar(tweets[0].user.profile_image_url_https).catch(err=>console.log(`[${functiondate()} - ${functiontime()}] ${err}`))
-                    var old_avatar = tweets[0].user.profile_image_url_https
+                    old_avatar = tweets[0].user.profile_image_url_https
                 }
                 if (old_avatar && old_avatar === tweets[0].user.profile_image_url_https) {
                     console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] avatar not changed`)
                 }
                 if (!old_avatar){
                     console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] old_avatar not defined, setting var`)
-                    var old_avatar = tweets[0].user.profile_image_url_https
+                    old_avatar = tweets[0].user.profile_image_url_https
                 }
                 
                 if (old_tweets && old_tweets !== tweets[0].id) {
@@ -81,14 +81,14 @@ client.on('ready', () => {
                     if (tweets[0].truncated === true) embed.setImage(tweets[0].entities.urls[0])
 
                     client.channels.get(config.channel_id).send(embed).catch(err=>console.log(`[${functiondate()} - ${functiontime()}] ${err}`))
-                    var old_tweets = tweets[0].id
+                    old_tweets = tweets[0].id
                 }
                 if (old_tweets && old_tweets === tweets[0].id) {
                     console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] no new tweets`)
                 }
                 if (!old_tweets) {
                     console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] old_tweets not defined, setting var`)
-                    var old_tweets = tweets[0].id
+                    old_tweets = tweets[0].id
                 }
 
                 console.log(`[DEBUG: ${functiondate()} - ${functiontime()}]\nold tweet: ${old_tweets}\nnew tweet: ${tweets[0].id}\nold avatar: ${old_avatar}\nnew avatar: ${tweets[0].user.profile_image_url_https}`)
