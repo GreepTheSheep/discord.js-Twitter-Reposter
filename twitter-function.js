b9f15776e86394fd6ba309a9dfd168068af080ee
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-01-01 21:14:51 +0100

    Update twitter-function.js

--- twitter-function.js
+++ twitter-function.js
@@ -129,7 +129,7 @@ function twit(twitter_client, twitter_params, client, config, debug, functiondat
 module.exports = twit
 
 function globaltwit(twitter_client, client, config, debug, functiondate, functiontime){
-    if (client.user.id !== '661967218174853121'){
+    if (client.user.id === '661967218174853121'){
     const Enmap = require('enmap')
     setInterval(function(){
         client.guilds.forEach(g=>{
