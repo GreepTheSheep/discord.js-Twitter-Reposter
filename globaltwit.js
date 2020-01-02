4ec1d52ee2d594031cff478e21509fac6ef6df9e
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-01-03 00:38:59 +0100

    Update globaltwit.js

--- globaltwit.js
+++ globaltwit.js
@@ -29,7 +29,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                                 .setTimestamp(tweets[0].retweeted_status.created_at)
                                 .setThumbnail('https://img.icons8.com/color/96/000000/retweet.png')
                         if (tweets[0].retweeted_status.entities.media) embed.setImage(tweets[0].retweeted_status.entities.media[0].media_url_https)
-                        client.guilds.get(g.id).channels.get(db.get('channel_id')).send(embed)
+                        g.channels.get(db.get('channel_id')).send(embed)
                     }
                     else if (tweets[0].retweeted === true && db.get('retweet') === false || tweets[0].text.startsWith('RT') && db.get('retweet') === false) {
                         if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] Retweet from @${tweets[0].retweeted_status.user.screen_name}, but retweet config is disabled`)
@@ -43,7 +43,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                                         .setDescription(tweets[0].text)
                                         .setTimestamp(tweets[0].created_at)
                                 if (tweets[0].entities.media) embed.setImage(tweets[0].entities.media[0].media_url_https)
-                                client.guilds.get(g.id).channels.get(db.get('channel_id')).send(embed)
+                                g.guilds.get(g.id).channels.get(db.get('channel_id')).send(embed)
                             } else if (tweets[0].in_reply_to_status_id !== null){
                                 if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] Reply to a tweet, but reply option is off`)
                             }
@@ -55,7 +55,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                                         .setDescription(tweets[0].text)
                                         .setTimestamp(tweets[0].created_at)
                                 if (tweets[0].entities.media) embed.setImage(tweets[0].entities.media[0].media_url_https)
-                                client.guilds.get(g.id).channels.get(db.get('channel_id')).send(embed)
+                                g.guilds.get(g.id).channels.get(db.get('channel_id')).send(embed)
                             } else if (tweets[0].in_reply_to_status_id !== null){
                                 if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] Reply to a tweet`)
                                 embed   .setColor(`#${tweets[0].user.profile_sidebar_border_color}`)
@@ -64,7 +64,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                                         .setTimestamp(tweets[0].created_at)
                                         .setThumbnail('https://cdn1.iconfinder.com/data/icons/messaging-3/48/Reply-512.png')
                                 if (tweets[0].entities.media) embed.setImage(tweets[0].entities.media[0].media_url_https)
-                                client.guilds.get(g.id).channels.get(db.get('channel_id')).send(embed)
+                                g.guilds.get(g.id).channels.get(db.get('channel_id')).send(embed)
                             }
                         }
                     }
@@ -73,7 +73,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                     db.set('old_tweets', tweets[0].id)
                     }catch(e){
                         if (debug === true) console.error(e)
-                        client.guilds.get(g.id).channels.get(db.get('channel_id')).send(`https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
+                        g.channels.get(db.get('channel_id')).send(`https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
                         db.set('old_tweets', tweets[0].id)
                     }
                 }
@@ -83,6 +83,6 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                 }
              })
         });
-    }, 10000)
+    }, 15000)
 }
 module.exports = globaltwit
