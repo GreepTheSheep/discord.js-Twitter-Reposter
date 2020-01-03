4524add1cf62fd8ca0ba29b40e22868434f24c93
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-01-04 00:16:51 +0100

    Update globaltwit.js

--- globaltwit.js
+++ globaltwit.js
@@ -17,13 +17,12 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                 }
                 if (db.has('old_tweets') && db.get('old_tweets') !== tweets[0].id) {
                     try{
-                        if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] new tweet! sending in Discord...`)
                 
                         let embed = new Discord.RichEmbed
 
                     if (tweets[0].retweeted === true || tweets[0].text.startsWith('RT')) {
                         if (db.get('retweet') === true){
-                            if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] Retweet from @${tweets[0].retweeted_status.user.screen_name}`)
+                            if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()} - guild ${g.id} ] Retweet from @${tweets[0].retweeted_status.user.screen_name}`)
                             embed   .setColor(`#${tweets[0].retweeted_status.user.profile_sidebar_border_color}`)
                                     .setAuthor(`Retweet\n${tweets[0].retweeted_status.user.name} (@${tweets[0].retweeted_status.user.screen_name})`, tweets[0].retweeted_status.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
                                     .setDescription(tweets[0].retweeted_status.text)
@@ -32,11 +31,11 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                             if (tweets[0].retweeted_status.entities.media) embed.setImage(tweets[0].retweeted_status.entities.media[0].media_url_https)
                             if (g.channels.find(c=>c.id == db.get('channel_id'))) g.channels.find(c=>c.id == db.get('channel_id')).send(embed)
                         } else {
-                            if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] Retweet from @${tweets[0].retweeted_status.user.screen_name}, but retweet config is disabled`)
+                            if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()} - guild ${g.id} ] Retweet from @${tweets[0].retweeted_status.user.screen_name}, but retweet config is disabled`)
                         }
                     } else if (tweets[0].retweeted === false || !tweets[0].text.startsWith('RT')) {
                         if (tweets[0].in_reply_to_status_id === null) {
-                            if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] Simple tweet`)
+                            if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()} - guild ${g.id} ] Simple tweet, id ${tweets[0].id_str}`)
                             embed   .setColor(`#${tweets[0].user.profile_sidebar_border_color}`)
                                     .setAuthor(`${tweets[0].user.name} (@${tweets[0].user.screen_name})`, tweets[0].user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
                                     .setDescription(tweets[0].text)
@@ -45,16 +44,20 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                             if (g.channels.find(c=>c.id == db.get('channel_id'))) g.channels.find(c=>c.id == db.get('channel_id')).send(embed)
                         } else if (tweets[0].in_reply_to_status_id !== null){
                             if (db.get('reply') === false){
-                                if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] Reply to a tweet, but reply option is off`)
+                                if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()} - guild ${g.id} ] Reply to a tweet, but reply option is off`)
                             } else {
-                                if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] Reply to a tweet`)
-                                embed   .setColor(`#${tweets[0].user.profile_sidebar_border_color}`)
-                                        .setAuthor(`${tweets[0].user.name} (@${tweets[0].user.screen_name})\nReply to ${tweets[0].entities.user_mentions[0].name} (@${tweets[0].entities.user_mentions[0].screen_name})`, tweets[0].user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
-                                        .setDescription(tweets[0].text.replace(`@${tweets[0].entities.user_mentions[0].screen_name}`, ""))
-                                        .setTimestamp(tweets[0].created_at)
-                                        .setThumbnail('https://cdn1.iconfinder.com/data/icons/messaging-3/48/Reply-512.png')
-                                if (tweets[0].entities.media) embed.setImage(tweets[0].entities.media[0].media_url_https)
-                                if (g.channels.find(c=>c.id == db.get('channel_id'))) g.channels.find(c=>c.id == db.get('channel_id')).send(embed)
+                                if (!db.has('reply_id')) db.set('reply_id', tweets[0].in_reply_to_status_id)
+                                if (db.get('reply_id') == tweets[0].in_reply_to_status_id){
+                                    if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()} - guild ${g.id} ] Reply to a tweet, id ${tweets[0].in_reply_to_status_id}`)
+                                    db.set('reply_id', tweets[0].in_reply_to_status_id)
+                                    embed   .setColor(`#${tweets[0].user.profile_sidebar_border_color}`)
+                                            .setAuthor(`${tweets[0].user.name} (@${tweets[0].user.screen_name})\nReply to ${tweets[0].entities.user_mentions[0].name} (@${tweets[0].entities.user_mentions[0].screen_name})`, tweets[0].user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
+                                            .setDescription(tweets[0].text.replace(`@${tweets[0].entities.user_mentions[0].screen_name}`, ""))
+                                            .setTimestamp(tweets[0].created_at)
+                                            .setThumbnail('https://cdn1.iconfinder.com/data/icons/messaging-3/48/Reply-512.png')
+                                    if (tweets[0].entities.media) embed.setImage(tweets[0].entities.media[0].media_url_https)
+                                    if (g.channels.find(c=>c.id == db.get('channel_id'))) g.channels.find(c=>c.id == db.get('channel_id')).send(embed)
+                                }
                             }
                         }
                     }
@@ -67,7 +70,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                     }
                 }
                 if (!db.has('old_tweets')) {
-                    if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()}] old_tweets not defined, setting var`)
+                    if (debug === true) console.log(`[DEBUG: ${functiondate()} - ${functiontime()} - guild ${g.id} ] old_tweets not defined, setting var`)
                     db.set('old_tweets', tweets[0].id)
                 }
              })
