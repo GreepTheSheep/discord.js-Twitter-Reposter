30fb5eafdc4e6f3ddf66bf39d55dc5e69a8ab476
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-01-21 11:08:41 +0100

    Update globaltwit.js

--- globaltwit.js
+++ globaltwit.js
@@ -14,7 +14,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                 if (err) client.shard.send(err);
 
                 if (db.has('old_tweets') && db.get('old_tweets') === tweets[0].id) {
-                    if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - Guild ${g.id} ] no new tweets`)
+                    if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - Guild ${g.id} (${g.name}) ] no new tweets`)
                 }
                 if (db.has('old_tweets') && db.get('old_tweets') !== tweets[0].id) {
                     try{
@@ -25,7 +25,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
 
                     if (tweets[0].retweeted === true || tweets[0].text.startsWith('RT')) {
                         if (db.get('retweet') === true){
-                            if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} ] Retweet from @${tweets[0].retweeted_status.user.screen_name}`)
+                            if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} (${g.name}) ] Retweet from @${tweets[0].retweeted_status.user.screen_name}`)
                             embed   .setColor(`#${tweets[0].retweeted_status.user.profile_sidebar_border_color}`)
                                     .setAuthor(`Retweet\n${tweets[0].retweeted_status.user.name} (@${tweets[0].retweeted_status.user.screen_name})`, tweets[0].retweeted_status.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
                                     .setDescription(tweets[0].retweeted_status.text)
@@ -34,11 +34,11 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                             if (tweets[0].retweeted_status.entities.media) embed.setImage(tweets[0].retweeted_status.entities.media[0].media_url_https)
                             if (g.channels.find(c=>c.id == db.get('channel_id'))) g.channels.find(c=>c.id == db.get('channel_id')).send(embed)
                         } else {
-                            if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} ] Retweet from @${tweets[0].retweeted_status.user.screen_name}, but retweet config is disabled`)
+                            if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} (${g.name}) ] Retweet from @${tweets[0].retweeted_status.user.screen_name}, but retweet config is disabled`)
                         }
                     } else if (tweets[0].retweeted === false || !tweets[0].text.startsWith('RT')) {
                         if (tweets[0].in_reply_to_status_id == null || tweets[0].in_reply_to_user_id == null) {
-                            if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} ] Simple tweet, id ${tweets[0].id_str}`)
+                            if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} (${g.name}) ] Simple tweet, id ${tweets[0].id_str}`)
                             embed   .setColor(`#${tweets[0].user.profile_sidebar_border_color}`)
                                     .setAuthor(`${tweets[0].user.name} (@${tweets[0].user.screen_name})`, tweets[0].user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
                                     .setDescription(tweets[0].text)
@@ -47,9 +47,9 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                             if (g.channels.find(c=>c.id == db.get('channel_id'))) g.channels.find(c=>c.id == db.get('channel_id')).send(embed)
                         } else if (tweets[0].in_reply_to_status_id != null || tweets[0].in_reply_to_user_id != null){
                             if (db.get('reply') === false){
-                                if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} ] Reply to a tweet, but reply option is off`)
+                                if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} (${g.name}) ] Reply to a tweet, but reply option is off`)
                             } else {
-                                if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} ] Reply to a tweet, id ${tweets[0].in_reply_to_status_id}`)
+                                if (debug === true) client.shard.send(`[DEBUG: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} (${g.name}) ] Reply to a tweet, id ${tweets[0].in_reply_to_status_id}`)
                                 embed   .setColor(`#${tweets[0].user.profile_sidebar_border_color}`)
                                         .setAuthor(`${tweets[0].user.name} (@${tweets[0].user.screen_name})\nReply to @${tweets[0].in_reply_to_screen_name}`, tweets[0].user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
                                         .setDescription(tweets[0].text.replace(`@${tweets[0].in_reply_to_screen_name}`, ""))
@@ -62,7 +62,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
                     }
                     db.set('old_tweets', tweets[0].id)
                     }catch(e){
-                        if (debug === true) client.shard.send(`[ERROR: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} ] ` + e)
+                        if (debug === true) client.shard.send(`[ERROR: ${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - guild ${g.id} (${g.name}) ] ` + e)
                         if (debug === true) client.shard.send(tweets[0])
                         if (g.channels.find(c=>c.id == db.get('channel_id'))) g.channels.find(c=>c.id == db.get('channel_id')).send(`https://twitter.com/${tweets[0].user.screen_name}/status/${tweets[0].id_str}`)
                         .catch(err=>client.shard.send(`Error sending on guild ${g.id} - ${g.name}\n${err}`))
