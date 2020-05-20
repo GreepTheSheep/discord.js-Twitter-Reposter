5d718c88d3e87e24c51eadaa0b620ba800e45efe
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-05-20 09:37:46 +0200

    Update globaltwit.js

--- globaltwit.js
+++ globaltwit.js
@@ -128,7 +128,7 @@ function globaltwit(twitter_client, client, config, debug, functiondate, functio
     } catch (e) {
         client.shard.send(`[${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - Guild ${g.id} (${g.name}) ] globaltwit interval function error:` + e);
     }
-    }, 1 * 60 * 1000) // 1min
+    }, 20 * 1000) // 20 sec
     
     } catch (e) {
         client.shard.send(`[${functiondate()} - ${functiontime()} - Shard ${client.shard.id + 1} - Guild ${g.id} (${g.name}) ] globaltwit function error:` + e);
