6a02b97814a82b4c7c3a9a0a5f2f18684f5dcf71
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-06-21 23:15:58 +0200

    Update globaltwit.js

--- globaltwit.js
+++ globaltwit.js
@@ -32,7 +32,7 @@ function globaltwit(twitter_client, tokens, client, config, debug, functiondate,
                 })
             }
             twitter_ids.push(account.twitter_id)
-            account_params[account.name].push(account)
+            account_params[account.twitter_id].push(account)
         })
     });
     const Tstream = twitter_client.stream("statuses/filter", { follow: twitter_ids })
@@ -65,7 +65,7 @@ function globaltwit(twitter_client, tokens, client, config, debug, functiondate,
                         })
                     }
                     twitter_ids.push(account.twitter_id)
-                    account_params[account.name].push(account)
+                    account_params[account.twitter_id].push(account)
                 })
             });
             // recreate new stream
@@ -85,108 +85,117 @@ function globaltwit(twitter_client, tokens, client, config, debug, functiondate,
     Tstream.on('data', async function (tweet) {
         try{
 
-            let embed = new Discord.RichEmbed
+            client.guilds.forEach(g=>{
+                var db = new Enmap({name:'db_'+g.id})
+                var twitter_accounts = db.has('twitter_name') ? db.get('twitter_name') : undefined
+                if (twitter_accounts === undefined) return
+                twitter_accounts.forEach(async account=>{
+                    if (account.twitter_id == tweet.user.id_str) {
+                        
+                    let embed = new Discord.RichEmbed
     
-            tweet.text.replace('&amp;', '&')
-    
-            if (tweet.retweeted === true || tweet.text.startsWith('RT')) {
-                if (account_params[tweet.user.screen_name].retweet === true){
-                    if (debug === true) client.shard.send(debug_header + `Retweet from @${tweet.retweeted_status.user.screen_name}`)
-                    embed   .setColor(account_params[tweet.user.screen_name].embed_color ? account_params[tweet.user.screen_name].embed_color : 'RANDOM')
-                            .setAuthor(`Retweet\n${tweet.retweeted_status.user.name} (@${tweet.retweeted_status.user.screen_name})`, tweet.retweeted_status.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
-                            .setDescription(tweet.retweeted_status.text)
-                            .setTimestamp(tweet.retweeted_status.created_at)
-                            .setThumbnail('https://img.icons8.com/color/96/000000/retweet.png')
-                    if (tweet.retweeted_status.entities.media) embed.setImage(tweet.retweeted_status.entities.media[0].media_url_https)
-                    if (client.channels.some(c=>c.id == account_params[tweet.user.screen_name].channel)) {
-                        var webhooks = await client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).fetchWebhooks()
-                        .catch(client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).createWebhook(client.user.username)
-                        .then(async wh=>{
-                                client.shard.send(`Created webhook ${wh.name} for account @${tweet.user.screen_name} on channel ${wh.channelID}`)
-                                webhook.send('', {
-                                    username: tweet.user.name,
-                                    avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
-                                    embeds: [embed]
-                                })
-                            })
-                        )
-                        webhooks = await client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).fetchWebhooks()
-                        var webhook = webhooks.first()
-                        webhook.send('', {
-                            username: tweet.user.name,
-                            avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
-                            embeds: [embed]
-                        })
-                    } else return
-                } else {
-                    if (debug === true) client.shard.send(debug_header + `Retweet from @${tweet.retweeted_status.user.screen_name}, but retweet config is disabled`)
-                }
-            } else if (tweet.retweeted === false || !tweet.text.startsWith('RT')) {
-                if (tweet.in_reply_to_status_id == null || tweet.in_reply_to_user_id == null) {
-                    if (debug === true) client.shard.send(debug_header + `Simple tweet, id ${tweet.id_str}`)
-                    embed   .setColor(account_params[tweet.user.screen_name].embed_color ? account_params[tweet.user.screen_name].embed_color : 'RANDOM')
-                            .setAuthor(`${tweet.user.name} (@${tweet.user.screen_name})`, tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
-                            .setDescription(tweet.text)
-                            .setTimestamp(tweet.created_at)
-                    if (tweet.entities.media) embed.setImage(tweet.entities.media[0].media_url_https)
-                    if (client.channels.some(c=>c.id == account_params[tweet.user.screen_name].channel)) {
-                        var webhooks = await client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).fetchWebhooks()
-                        .catch(client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).createWebhook(client.user.username)
-                            .then(async wh=>{
-                                client.shard.send(`Created webhook ${wh.name} for account @${tweet.user.screen_name} on channel ${wh.channelID}`)
-                                webhook.send('', {
-                                    username: tweet.user.name,
-                                    avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
-                                    embeds: [embed]
-                                })
-                            })
-                        )
-                        webhooks = await client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).fetchWebhooks()
-                        var webhook = webhooks.first()
-                        webhook.send('', {
-                            username: tweet.user.name,
-                            avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
-                            embeds: [embed]
-                        })
-                    } else return
-                } else if (tweet.in_reply_to_status_id != null || tweet.in_reply_to_user_id != null){
-                    if (account_params[tweet.user.screen_name].reply === false){
-                        if (debug === true) client.shard.send(debug_header + `Reply to a tweet, but reply option is off`)
-                    } else {
-                        if (debug === true) client.shard.send(debug_header + `Reply to a tweet, id ${tweet.in_reply_to_status_id}`)
-                        embed   .setColor(account_params[tweet.user.screen_name].embed_color ? account_params[tweet.user.screen_name].embed_color : 'RANDOM')
-                                .setAuthor(`${tweet.user.name} (@${tweet.user.screen_name})\nReply to @${tweet.in_reply_to_screen_name}`, tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
-                                .setDescription(tweet.text.replace(`@${tweet.in_reply_to_screen_name}`, ""))
-                                .setTimestamp(tweet.created_at)
-                                .setThumbnail('https://cdn1.iconfinder.com/data/icons/messaging-3/48/Reply-512.png')
-                        if (tweet.entities.media) embed.setImage(tweet.entities.media[0].media_url_https)
-                        if (client.channels.some(c=>c.id == account_params[tweet.user.screen_name].channel)) {
-                            var webhooks = await client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).fetchWebhooks()
-                            .catch(client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).createWebhook(client.user.username)
-                                .then(async wh=>{
-                                    client.shard.send(`Created webhook ${wh.name} for account @${tweet.user.screen_name} on channel ${wh.channelID}`)
+                    tweet.text.replace('&amp;', '&')
+                        if (tweet.retweeted === true || tweet.text.startsWith('RT')) {
+                            if (account.retweet === true){
+                                if (debug === true) client.shard.send(debug_header + `Retweet from @${tweet.retweeted_status.user.screen_name}`)
+                                embed   .setColor(account.embed_color ? account.embed_color : 'RANDOM')
+                                        .setAuthor(`Retweet\n${tweet.retweeted_status.user.name} (@${tweet.retweeted_status.user.screen_name})`, tweet.retweeted_status.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
+                                        .setDescription(tweet.retweeted_status.text)
+                                        .setTimestamp(tweet.retweeted_status.created_at)
+                                        .setThumbnail('https://img.icons8.com/color/96/000000/retweet.png')
+                                if (tweet.retweeted_status.entities.media) embed.setImage(tweet.retweeted_status.entities.media[0].media_url_https)
+                                if (client.channels.some(c=>c.id == account.channel)) {
+                                    var webhooks = await client.channels.find(c=>c.id == account.channel).fetchWebhooks()
+                                    .catch(client.channels.find(c=>c.id == account.channel).createWebhook(client.user.username)
+                                    .then(async wh=>{
+                                            client.shard.send(`Created webhook ${wh.name} for account @${tweet.user.screen_name} on channel ${wh.channelID}`)
+                                            webhook.send('', {
+                                                username: tweet.user.name,
+                                                avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
+                                                embeds: [embed]
+                                            })
+                                        })
+                                    )
+                                    webhooks = await client.channels.find(c=>c.id == account.channel).fetchWebhooks()
+                                    var webhook = webhooks.first()
                                     webhook.send('', {
                                         username: tweet.user.name,
                                         avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
                                         embeds: [embed]
                                     })
-                                })
-                            )
-                            webhooks = await client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).fetchWebhooks()
-                            var webhook = webhooks.first()
-                            webhook.send('', {
-                                username: tweet.user.name,
-                                avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
-                                embeds: [embed]
-                            })
-                        } else return
+                                } else return
+                            } else {
+                                if (debug === true) client.shard.send(debug_header + `Retweet from @${tweet.retweeted_status.user.screen_name}, but retweet config is disabled`)
+                            }
+                        } else if (tweet.retweeted === false || !tweet.text.startsWith('RT')) {
+                            if (tweet.in_reply_to_status_id == null || tweet.in_reply_to_user_id == null) {
+                                if (debug === true) client.shard.send(debug_header + `Simple tweet, id ${tweet.id_str}`)
+                                embed   .setColor(account.embed_color ? account.embed_color : 'RANDOM')
+                                        .setAuthor(`${tweet.user.name} (@${tweet.user.screen_name})`, tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
+                                        .setDescription(tweet.text)
+                                        .setTimestamp(tweet.created_at)
+                                if (tweet.entities.media) embed.setImage(tweet.entities.media[0].media_url_https)
+                                if (client.channels.some(c=>c.id == account.channel)) {
+                                    var webhooks = await client.channels.find(c=>c.id == account.channel).fetchWebhooks()
+                                    .catch(client.channels.find(c=>c.id == account.channel).createWebhook(client.user.username)
+                                        .then(async wh=>{
+                                            client.shard.send(`Created webhook ${wh.name} for account @${tweet.user.screen_name} on channel ${wh.channelID}`)
+                                            webhook.send('', {
+                                                username: tweet.user.name,
+                                                avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
+                                                embeds: [embed]
+                                            })
+                                        })
+                                    )
+                                    webhooks = await client.channels.find(c=>c.id == account.channel).fetchWebhooks()
+                                    var webhook = webhooks.first()
+                                    webhook.send('', {
+                                        username: tweet.user.name,
+                                        avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
+                                        embeds: [embed]
+                                    })
+                                } else return
+                            } else if (tweet.in_reply_to_status_id != null || tweet.in_reply_to_user_id != null){
+                                if (account.reply === false){
+                                    if (debug === true) client.shard.send(debug_header + `Reply to a tweet, but reply option is off`)
+                                } else {
+                                    if (debug === true) client.shard.send(debug_header + `Reply to a tweet, id ${tweet.in_reply_to_status_id}`)
+                                    embed   .setColor(account.embed_color ? account.embed_color : 'RANDOM')
+                                            .setAuthor(`${tweet.user.name} (@${tweet.user.screen_name})\nReply to @${tweet.in_reply_to_screen_name}`, tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"), `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
+                                            .setDescription(tweet.text.replace(`@${tweet.in_reply_to_screen_name}`, ""))
+                                            .setTimestamp(tweet.created_at)
+                                            .setThumbnail('https://cdn1.iconfinder.com/data/icons/messaging-3/48/Reply-512.png')
+                                    if (tweet.entities.media) embed.setImage(tweet.entities.media[0].media_url_https)
+                                    if (client.channels.some(c=>c.id == account.channel)) {
+                                        var webhooks = await client.channels.find(c=>c.id == account.channel).fetchWebhooks()
+                                        .catch(client.channels.find(c=>c.id == account.channel).createWebhook(client.user.username)
+                                            .then(async wh=>{
+                                                client.shard.send(`Created webhook ${wh.name} for account @${tweet.user.screen_name} on channel ${wh.channelID}`)
+                                                webhook.send('', {
+                                                    username: tweet.user.name,
+                                                    avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
+                                                    embeds: [embed]
+                                                })
+                                            })
+                                        )
+                                        webhooks = await client.channels.find(c=>c.id == account.channel).fetchWebhooks()
+                                        var webhook = webhooks.first()
+                                        webhook.send('', {
+                                            username: tweet.user.name,
+                                            avatarURL: tweet.user.profile_image_url_https.replace("normal.jpg", "200x200.jpg"),
+                                            embeds: [embed]
+                                        })
+                                    } else return
+                                }
+                            }
+                        }
                     }
-                }
-            }
+                })
+            })
         }catch(e){
             if (debug === true) client.shard.send(`ERROR: ${debug_header}` + e)
             if (debug === true) client.shard.send(tweet)
-            if (client.channels.some(c=>c.id == account_params[tweet.user.screen_name].channel)) client.channels.find(c=>c.id == account_params[tweet.user.screen_name].channel).send(`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
+            if (client.channels.some(c=>c.id == account.channel)) client.channels.find(c=>c.id == account.channel).send(`https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`)
             .catch(err=>client.shard.send(`Error sending on guild ${g.id} - ${g.name}\n${err}`))               
         }
     })
