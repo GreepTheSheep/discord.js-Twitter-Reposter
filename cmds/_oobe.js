7ffc48963009e8916d4588f4d280804c9f8d3023
Matthieu <42576124+GreepTheSheep@users.noreply.github.com>
2020-06-22 17:36:54 +0200

    up

--- cmds/_oobe.js
+++ cmds/_oobe.js
@@ -137,12 +137,11 @@ async function oobe_stepByStep(message, client, config, functiondate, functionti
                                             
                                             db.set('shard_id', client.shard.id + 1)
 
+                                            newaccs.emit('basicEvent')
                                             bm.edit(`Ok, ${rptextbm}.\n\nThe setting is now done. You can now enjoy the power of Twitter reposting!`)
                                         
                                             const check_number_of_accounts = require('../events/check_number.js')
                                             check_number_of_accounts(client, config, debug, functiondate, functiontime, twit_send)
-
-                                            newaccs.emit('basicEvent')
                                         
                                         });
                                         collector6.on('end', (collected, reason) => {
