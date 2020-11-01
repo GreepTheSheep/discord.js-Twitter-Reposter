36f4cb53d648abe6230559317be81b9d8f07f6c3
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2020-11-01 01:26:18 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   apps : [{
     name: 'Twitter-Discord',
-    script: 'shard.js',
+    script: 'index.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    args: 'one two',
+    args: '',
     instances: 1,
     autorestart: true,
     watch: false,
